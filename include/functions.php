de8660ce8b5c64db7c75b9b3dc901f1768e4849d
Dylan Bos <dylanbos1996@gmail.com>
2018-11-28 11:50:30 +0100

    UPDATE

--- include/functions.php
+++ include/functions.php
@@ -1,5 +1,5 @@
 <?php
-function dump($str, $jslog = 0, $jsdel = 0){
+function dump($str, $show = "active ", $framework = "materialize"){
     $bt = debug_backtrace();
     $caller = array_shift($bt);
     $tokens = explode('\\', $caller['file']);
@@ -7,14 +7,29 @@ function dump($str, $jslog = 0, $jsdel = 0){
     $readFileStr = file($caller['file']);
     $lineStr = $readFileStr[$caller['line'] -1];
     $regularOutput = preg_match('/\((.*?)\)/', $lineStr, $output);
-    $variableName = str_replace([", 1", ",1"], "", $output[1]);
-    $extraStr = "";
-    if ($jsdel) {
-        $extraStr = "js-delete-this-log";
+    $variableName = str_replace([", 1", ",1", '""', ", "], "", $output[1]);
+    $random_digit = mt_rand(10000000, 99999999);
+    switch ($framework) {
+      case 'bootstrap':
+        echo '<button class="btn btn-light" data-toggle="collapse" data-target="#' . $random_digit . '">' .$variableName . '</button>';
+        echo "<div class='collapse " . $show ."' id='" . $random_digit . "'>";
+        echo "var '<strong>" . $variableName . "</strong>' dumped in file: <strong>" . $file . '</strong> on line: <strong>' . $caller['line'] . '</strong><br>';
+        echo '<pre>' . var_export($str, true) . '</pre>';
+        echo "</div>";
+        break;
+      case 'materialize':
+          echo '<ul class="collapsible">';
+          echo '  <li class="' . $show . '">';
+          echo '    <div class="collapsible-header">' .$variableName .'</div>';
+          echo '    <div class="collapsible-body">';
+          echo "var '<strong>" . $variableName . "</strong>' dumped in file: <strong>" . $file . '</strong> on line: <strong>' . $caller['line'] . '</strong><br>';
+          echo '      <pre>' . var_export($str, true) . '</pre>';
+          echo '    </div>';
+          echo '  </li>';
+          echo '</ul>';
+        break;
+      default:
+
+        break;
     }
-    echo $jslog ? "<div class='js-log " . $extraStr . "'>" : "<div>";
-    echo "var '<strong>" . $variableName . "</strong>' dumped in file: <strong>" . $file . '</strong> on line: <strong>' . $caller['line'] . '</strong><br>';
-    echo '<pre>' . var_export($str, true) . '</pre>';
-    echo "</div>";
 }
-?>
