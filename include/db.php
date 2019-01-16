315cf9aaf7c343dbe7c4bf1a0b0edada1fb98408
db1996 <dylanbos1996@gmail.com>
2019-01-16 15:04:49 +0100

    added db class

---
+++ include/db.php
@@ -0,0 +1,70 @@
+<?php
+class db {
+
+  var $con;
+  var $query;
+  var $stmt;
+  var $insertid;
+  var $executed = false;
+
+  function __construct($con)
+  {
+    $this->con = $con;
+  }
+
+  function setQuery($q){
+    $this->query = $q;
+  }
+
+  function prepare($q = ""){
+    if ($q == "") {
+      $q = $this->query;
+    }
+    else {
+      $this->query = $q;
+    }
+
+    if (false === ($this->stmt = $this->con->prepare($q))) {
+      echo 'error preparing statement: ' . $con->error;
+    }
+  }
+
+  function bindParam($s, ...$params){
+    if (false === ($this->stmt->bind_param($s, ...$params))) {
+      echo 'error binding params to statement: ' . $this->con->error;
+    }
+  }
+
+  function execute(){
+    if (false === ($this->stmt->execute())) {
+      echo 'error executing statement: ' . $this->stmt->error;
+    }
+    else{
+      $this->insertid = $this->stmt->insert_id;
+      $this->executed = true;
+    }
+  }
+
+  function close(){
+    $this->stmt->close();
+  }
+
+  function getResult(){
+    $e = $this->executed;
+    if (!$e) {
+      $this->execute();
+    }
+    $result = $this->stmt->get_result();
+    $returnAr = [];
+    while ($row = $result->fetch_array(MYSQLI_ASSOC))
+    {
+      $returnAr[] = $row;
+    }
+    return $returnAr;
+  }
+
+  function insert_id(){
+    return $this->insertid;
+  }
+}
+?>
