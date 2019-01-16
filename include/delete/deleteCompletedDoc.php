883b57cafb1c194866a008c273410c36419fac46
db1996 <dylanbos1996@gmail.com>
2019-01-16 12:06:31 +0100

    fixed convert to docx

--- include/delete/deleteCompletedDoc.php
+++ include/delete/deleteCompletedDoc.php
@@ -1,9 +1,9 @@
 <?php
-define("APPLICATION_PATH",  str_replace("include\\pdf","",dirname(__FILE__)));
+// After downloading file as a DOCX, the temporary file is deleted
+define("APPLICATION_PATH",  str_replace("include\\delete","",dirname(__FILE__)));
 $directory = APPLICATION_PATH;
 $file = $_POST['file'];
 if(is_file($directory . "\\_completed\\" . $file . ".docx")){
   unlink($directory . "\\_completed\\" . $file . ".docx"); // delete file
-  echo " success";
 }
 ?>
--- include/pdf/downloadPdfDoc.php
+++ include/pdf/downloadPdfDoc.php
@@ -2,17 +2,18 @@
 $file = $_POST['file'];
 define("APPLICATION_PATH",  str_replace("include\\pdf","",dirname(__FILE__)));
 $directory = APPLICATION_PATH;
-// die();
-$word = new COM("Word.Application") or die ("Could not initialise Object.");
-// set it to 1 to see the MS Word window (the actual opening of the document)
-$word->Visible = 0;
-// recommend to set to 0, disables alerts like "Do you want MS Word to be the default .. etc"
-$word->DisplayAlerts = 0;
-$word->Documents->Open($directory . '\\_completed\\' . $file . ".pdf", false, true, false);
-$word->ActiveDocument->SaveAs($directory . '\\_completed\\' . $file . '.docx');
+if (!file_exists($directory . '\\_completed\\' . $file . '.docx')) {
+  $word = new COM("Word.Application") or die ("Could not initialise Object.");
+  // set it to 1 to see the MS Word window (the actual opening of the document)
+  $word->Visible = 0;
+  // recommend to set to 0, disables alerts like "Do you want MS Word to be the default .. etc"
+  $word->DisplayAlerts = 0;
+  $word->Documents->Open($directory . '\\_completed\\' . $file . ".pdf", false, true, false);
+  $word->ActiveDocument->SaveAs($directory . '\\_completed\\' . $file . '.docx');
 
-$word->Quit(false);
-// clean up
-unset($word);
+  $word->Quit(false);
+  // clean up
+  unset($word);
+}
 echo "success";
 ?>
